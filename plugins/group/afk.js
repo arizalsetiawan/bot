1f95bb0503a56ef7e6655209a3022c36d59304b0
arizalsetiawan <44498602+arizalsetiawan@users.noreply.github.com>
2023-05-27 10:49:43 +0700

    Add files via upload

---
+++ plugins/group/afk.js
@@ -0,0 +1,21 @@
+exports.run = {
+   usage: ['afk'],
+   use: 'reason (optional)',
+   category: 'group',
+   async: async (m, {
+      client,
+      text
+   }) => {
+      try {
+         let user = global.db.users.find(v => v.jid == m.sender)
+         user.afk = +new Date
+         user.afkReason = text
+         let tag = m.sender.split`@` [0]
+         return client.reply(m.chat, Func.texted('bold', `🚩 @${tag} Sedang AFK! Jangan diganggu, Tunggu sampai online kembali`), m)
+      } catch {
+         client.reply(m.chat, global.status.error, m)
+      }
+   },
+   error: false,
+   group: true
+}
